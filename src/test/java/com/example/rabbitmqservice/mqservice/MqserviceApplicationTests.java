df5b25ee5662c38672444084918117be8e92e19b
Nicolaas Vercuiel <nicolaas.vercuiel@sqn.world>
2020-08-13 23:53:45 +0200

    First commit

---
+++ src/test/java/com/example/rabbitmqservice/mqservice/MqserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqservice.mqservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
